23cc537114480755e1c7f160085e74a6b10baeb8
kms199406 <sksdmltkek14@gmail.com>
2024-02-08 19:12:03 +0900

    첫번째 커밋

---
+++ src/main/java/home/zigzag_project/ZigzagProjectApplication.java
@@ -0,0 +1,13 @@
+package home.zigzag_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigzagProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZigzagProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/home/zigzag_project/ZigzagProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package home.zigzag_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigzagProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
